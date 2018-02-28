a1e60b15ead52bc1b1badb1f38e5241c59cbcd41
Alban <33512013+Alban13@users.noreply.github.com>
2018-02-28 12:29:15 +0100

    v1

---
+++ src/app/article.ts
@@ -0,0 +1,18 @@
+import * as moment from 'moment';
+
+
+export class Article {
+    id: number;
+    title: string;
+    description: string;
+    author: string;
+    date: moment.Moment;
+
+    constructor(id: number, title: string, description?: string, author: string = 'alban'){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.date = moment();
+    }
+}
